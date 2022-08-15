779c980a4cfecbdc0b9cb1af5b29cfa07c4a1dd9
Luka Gogiashvili <luka.gogiashvili.02@gmail.com>
2022-08-15 16:40:22 +0400

    created router and outler

---
+++ frontend/src/layouts/AuthorizedLayout.jsx
@@ -0,0 +1,31 @@
+//  REACT
+import React, { useEffect } from "react";
+import { useNavigate, Outlet } from "react-router-dom";
+// import { useSelector } from "react-redux";
+
+// import Header from "../components/Header";
+// import Back from "../components/Back";
+
+function AuthorizedLayout() {
+  const navigate = useNavigate();
+  //   const userLogin = useSelector((state) => state.userLogin);
+  //   const { userInfo } = userLogin;
+
+  //   useEffect(() => {
+  //     if (!userInfo) {
+  //       navigate("/login");
+  //     }
+  //   }, []);
+  return (
+    // <>
+    //   {userInfo && (
+        <>
+          {/* <Header /> */}
+          <Outlet />
+        </>
+    //   )}
+    // </>
+  );
+}
+
+export default AuthorizedLayout;
---
+++ frontend/src/layouts/HomeLayout.jsx
@@ -0,0 +1,20 @@
+// REACT
+import React, { useEffect } from "react";
+import { useNavigate, Outlet } from "react-router-dom";
+// import { useSelector } from "react-redux";
+
+function HomeLayout() {
+  const navigate = useNavigate();
+  //   const userLogin = useSelector((state) => state.userLogin);
+  //   const { userInfo } = userLogin;
+
+  //   useEffect(() => {
+  //     if (userInfo) {
+  //       navigate(-1, { replace: true });
+  //     }
+  //   }, []);
+
+  return <Outlet />;
+}
+
+export default HomeLayout;
