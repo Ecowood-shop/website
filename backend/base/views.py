a6548d51a8fcde53fde432f89b0a736b677cbacd
temopkhakadze <73776293+temopkhakadze@users.noreply.github.com>
2022-08-15 21:36:39 +0400

    Update Database Details

--- backend/base/views.py
+++ backend/base/views.py
@@ -4,7 +4,9 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-# from base.models import Product, Review
+from .models import Product
+
+from .serializers import ProductSerializer
 
 
 # Create your views here.
@@ -24,11 +26,17 @@ def getRoutes(request):
     return Response(routes)
 
 
-# @api_view(['GET'])
-# def getProductCategories(request):
-#     categories = []
-#     for product in Product.objects.all():
-#         categories.append(product.category)
-#     categories = list(dict.fromkeys(categories))
-#
-#     return Response(categories)
+@api_view(['GET'])
+def getProducts(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def getProduct(request, pk):
+    product = Product.objects.get(_id=pk)
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
+
+
